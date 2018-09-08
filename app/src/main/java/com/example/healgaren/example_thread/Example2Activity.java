cc941618f187ef5446b5d516aa4b502691f55122
HealGaren <healgaren99@gmail.com>
2018-09-08 22:18:15 +0900

    예제2 이미지 일시정지, 재시작 동작하도록 코드 수정

--- app/src/main/java/com/example/healgaren/example_thread/Example2Activity.java
+++ app/src/main/java/com/example/healgaren/example_thread/Example2Activity.java
@@ -3,8 +3,10 @@ package com.example.healgaren.example_thread;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Example2Activity extends AppCompatActivity {
 
@@ -12,6 +14,21 @@ public class Example2Activity extends AppCompatActivity {
     Button pauseBtn, restartBtn;
 
     Handler handler = new Handler();
+
+    final static int STATUS_LOOP = 1;
+    final static int STATUS_PAUSE = 2;
+
+    int loopStatus = STATUS_LOOP;
+
+    Runnable onChangeToNextImage = new Runnable() {
+        @Override
+        public void run() {
+            currentImageIndex++;
+            if (currentImageIndex >= SHUFFLE_IMAGE_RESOURCE_ARRAY.length) currentImageIndex = 0;
+            shuffleImg.setImageResource(SHUFFLE_IMAGE_RESOURCE_ARRAY[currentImageIndex]);
+            postNextImage();
+        }
+    };
 
     int currentImageIndex = 0;
 
@@ -32,18 +49,38 @@ public class Example2Activity extends AppCompatActivity {
         pauseBtn = findViewById(R.id.btn_pause);
         restartBtn = findViewById(R.id.btn_restart);
 
+        pauseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (loopStatus == STATUS_LOOP) pauseImageShuffle();
+                else
+                    Toast.makeText(Example2Activity.this, "이미 중지되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        restartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (loopStatus == STATUS_PAUSE) startLoopNextImage();
+                else
+                    Toast.makeText(Example2Activity.this, "이미 시작되었습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        startLoopNextImage();
+    }
+
+    private void startLoopNextImage() {
         postNextImage();
+        loopStatus = STATUS_LOOP;
     }
 
     private void postNextImage() {
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                currentImageIndex++;
-                if (currentImageIndex >= SHUFFLE_IMAGE_RESOURCE_ARRAY.length) currentImageIndex = 0;
-                shuffleImg.setImageResource(SHUFFLE_IMAGE_RESOURCE_ARRAY[currentImageIndex]);
-                postNextImage();
-            }
-        }, 100);
+        handler.postDelayed(onChangeToNextImage, 100);
+    }
+
+    private void pauseImageShuffle() {
+        handler.removeCallbacks(onChangeToNextImage);
+        loopStatus = STATUS_PAUSE;
     }
 }
