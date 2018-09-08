296063c4e29be586a12e64aca20a72525650bd06
HealGaren <healgaren99@gmail.com>
2018-09-08 21:10:10 +0900

    NextActivity에서 버튼을 누르면 1초 뒤에 현재 시각 표시

--- app/src/main/java/com/example/healgaren/example_thread/NextActivity.java
+++ app/src/main/java/com/example/healgaren/example_thread/NextActivity.java
@@ -1,13 +1,39 @@
 package com.example.healgaren.example_thread;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class NextActivity extends AppCompatActivity {
+
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("hh시 mm분 ss초", Locale.KOREA);
+
+    Handler handler = new Handler();
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
+
+        text = findViewById(R.id.text);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        text.setText(simpleDateFormat.format(new Date()));
+                    }
+                }, 1000);
+            }
+        });
     }
 }
