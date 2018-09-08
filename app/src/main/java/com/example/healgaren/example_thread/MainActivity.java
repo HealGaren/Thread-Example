00e02afbf4a40e7fbe2e376a645ffc5ca85c1652
HealGaren <healgaren99@gmail.com>
2018-09-08 20:39:26 +0900

    1초 뒤에 스플래시가 꺼지는 기능 구현

--- app/src/main/java/com/example/healgaren/example_thread/MainActivity.java
+++ app/src/main/java/com/example/healgaren/example_thread/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.healgaren.example_thread;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                startActivity(new Intent(MainActivity.this, NextActivity.class));
+                finish();
+            }
+        }).start();
+
     }
 }
