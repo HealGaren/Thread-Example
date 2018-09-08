4bdc1a062a06fb5f9909dc4abcdd2836be711b07
HealGaren <healgaren99@gmail.com>
2018-09-08 20:49:18 +0900

    딜레이를 주는 방식을 쓰레드에서 Handler PostDelayed로 변경

--- app/src/main/java/com/example/healgaren/example_thread/MainActivity.java
+++ app/src/main/java/com/example/healgaren/example_thread/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.healgaren.example_thread;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,19 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        new Thread(new Runnable() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                startActivity(new Intent(MainActivity.this, NextActivity.class));
                 finish();
+                startActivity(new Intent(MainActivity.this, NextActivity.class));
             }
-        }).start();
+        }, 1000);
 
     }
 }
